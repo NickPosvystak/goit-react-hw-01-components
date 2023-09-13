7d053964066d068eb1d51b77b617ef2eb9097940
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2023-09-13 20:52:33 +0200

    update

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Profile from '../src/components/Profile';
+import user from '../src/data/user.json';
+
+export const App = () => {
+  return (
+    <>
+    <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </>
+  );
+};
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+
+const Profile = ({userName,tag,location, stats, avatar,}) => {
+    return (<div className="profile">
+      <div className="description">
+        <img src={avatar} alt="User avatar" className="avatar" width="250px" />
+        <p className="name">{userName}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      <ul className={stats}>
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>)
+}
+export default Profile;
